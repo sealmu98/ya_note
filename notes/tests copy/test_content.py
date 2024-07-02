b1dc123a9b7e31e542819c8b1e57384c8805ebf9
sealmu98 <seal.mu98@gmail.com>
2024-07-02 15:02:45 +0300

    tests_copy

---
+++ notes/tests copy/test_content.py
@@ -0,0 +1,38 @@
+# notes/tests/test_content.py
+from django.test import TestCase, Client
+from django.urls import reverse
+from notes.forms import NoteForm
+from notes.models import Note
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class TestContent(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author = User.objects.create(username='Автор')
+        self.not_author = User.objects.create(username='Не автор')
+        self.note = Note.objects.create(
+            title='Заголовок', text='Текст заметки', slug='note-slug', author=self.author)
+
+    def test_notes_list_for_different_users(self):
+        clients = [(self.author, True), (self.not_author, False)]
+        for client, note_in_list in clients:
+            self.client.force_login(client)
+            url = reverse('notes:list')
+            response = self.client.get(url)
+            object_list = response.context['object_list']
+            self.assertEqual((self.note in object_list), note_in_list)
+
+    def test_pages_contains_form(self):
+        pages = [
+            ('notes:add', None),
+            ('notes:edit', [self.note.slug]),
+        ]
+        for page, args in pages:
+            url = reverse(page, args=args)
+            self.client.force_login(self.author)
+            response = self.client.get(url)
+            self.assertIn('form', response.context)
+            self.assertIsInstance(response.context['form'], NoteForm)
---
+++ notes/tests copy/test_routes.py
@@ -0,0 +1,56 @@
+# notes/tests/test_routes.py
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from notes.models import Note
+
+
+class TestRoutes(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author = User.objects.create(username='Автор')
+        self.not_author = User.objects.create(username='Не автор')
+        self.note = Note.objects.create(
+            title='Заголовок', text='Текст заметки', slug='note-slug', author=self.author)
+
+    def test_pages_availability_for_anonymous_user(self):
+        pages = ['notes:home', 'users:login', 'users:logout', 'users:signup']
+        for page in pages:
+            url = reverse(page)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_pages_availability_for_auth_user(self):
+        self.client.force_login(self.not_author)
+        pages = ['notes:list', 'notes:add', 'notes:success']
+        for page in pages:
+            url = reverse(page)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_pages_availability_for_different_users(self):
+        pages = ['notes:detail', 'notes:edit', 'notes:delete']
+        for page in pages:
+            url = reverse(page, args=[self.note.slug])
+            self.client.force_login(self.not_author)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 404)
+            self.client.force_login(self.author)
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_redirects(self):
+        pages = [
+            ('notes:detail', [self.note.slug]),
+            ('notes:edit', [self.note.slug]),
+            ('notes:delete', [self.note.slug]),
+            ('notes:add', None),
+            ('notes:success', None),
+            ('notes:list', None),
+        ]
+        login_url = reverse('users:login')
+        for page, args in pages:
+            url = reverse(page, args=args)
+            expected_url = f'{login_url}?next={url}'
+            response = self.client.get(url)
+            self.assertRedirects(response, expected_url)
